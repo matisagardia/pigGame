2870e6a5d1eff7c8979ce0ff3547c866ed7566c1
matisagardia <matisagardia7@gmail.com>
2022-12-12 12:47:57 -0300

    buttons functionalities, roll, hold and new game

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
 // Variables declaration
 
+const player0 = document.querySelector(".player--0")
+const player1 = document.querySelector(".player--1")
 const scorePlayer0 = document.querySelector("#score--0");
 const scorePlayer1 = document.querySelector("#score--1");
 let current0 = document.querySelector("#current--0");
-const current1 = document.querySelector("#current--1");
+let current1 = document.querySelector("#current--1");
 const dice = document.querySelector(".dice");
 const newMatchBtn = document.querySelector(".btn--new");
 const rollBtn = document.querySelector(".btn--roll");
@@ -15,7 +17,9 @@ scorePlayer0.textContent = 0;
 scorePlayer1.textContent = 0;
 dice.classList.add('hidden');
 let currentScore = 0;
-
+let activePlayer = 0;
+let score0 = 0;
+let score1 = 0;
 
 
 // Roll the dice
@@ -28,13 +32,18 @@ rollBtn.addEventListener("click", () => {
 
     function diceToScore() {
         currentScore += diceNumber;
-        current0.textContent = currentScore;
+        document.querySelector(`#current--${activePlayer}`).textContent = currentScore;
     }
 
     // Conditionals for each number
     if(diceNumber == 1) {
         dice.src = './images/dice-1.png';
-        diceToScore();
+        currentScore = 0;
+        current0.textContent = currentScore;
+        current1.textContent = currentScore;
+        activePlayer = activePlayer === 0 ? 1 : 0;
+        player0.classList.toggle("player--active");
+        player1.classList.toggle("player--active");
     } else if(diceNumber == 2){
         dice.src = './images/dice-2.png';
         diceToScore();
@@ -51,4 +60,29 @@ rollBtn.addEventListener("click", () => {
         dice.src = './images/dice-6.png';
         diceToScore();
     }
+})
+
+
+holdBtn.addEventListener("click", () => {
+    activePlayer == 0 ? score0 += currentScore : score1 += currentScore;
+    scorePlayer0.textContent = score0;
+    scorePlayer1.textContent = score1;
+    activePlayer = activePlayer === 0 ? 1 : 0;
+    player0.classList.toggle("player--active");
+    player1.classList.toggle("player--active");
+    currentScore = 0;
+    current0.textContent = currentScore;
+    current1.textContent = currentScore;
+})
+
+newMatchBtn.addEventListener("click", () => {
+    scorePlayer0.textContent = 0;
+    scorePlayer1.textContent = 0;
+    dice.classList.add('hidden');
+    currentScore = 0;
+    current0.textContent = currentScore;
+    current1.textContent = currentScore;
+    activePlayer = 0;
+    score0 = 0;
+    score1 = 0;
 })
